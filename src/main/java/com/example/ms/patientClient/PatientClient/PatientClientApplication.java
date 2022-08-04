53761cfa5a4996bcd2c88a1de96093b96c0dc51c
rkRaichur123 <91364485+rkRaichur123@users.noreply.github.com>
2022-08-04 16:39:24 +0530

    API gatway integration

--- src/main/java/com/example/ms/patientClient/PatientClient/PatientClientApplication.java
+++ src/main/java/com/example/ms/patientClient/PatientClient/PatientClientApplication.java
@@ -1,13 +1,16 @@
 package com.example.ms.patientClient.PatientClient;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@RequestMapping("/patientClient")
+@EnableEurekaClient
 @SpringBootApplication
+@RequestMapping("/api/patient")
 public class PatientClientApplication {
 
 	public static void main(String[] args) {
